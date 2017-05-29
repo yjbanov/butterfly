71d24be91b59e42cfd6a4321a3d94167ac5ab39c
Jonah Williams <jonahwilliams@users.noreply.github.com>
2017-05-28 23:27:18 -0700

    added basic logging functionality (#16)

--- lib/dev_server.dart
+++ lib/dev_server.dart
@@ -18,6 +18,8 @@ import 'dart:io';
 
 import 'package:mime/mime.dart' as mime;
 import 'package:path/path.dart' as pathlib;
+import 'package:ansicolor/ansicolor.dart' as ansi;
+import 'package:logging/logging.dart';
 
 import 'butterfly.dart';
 
@@ -26,11 +28,13 @@ class ButterflyDevServer {
   static const String _devChannelNamespace = '_butterfly';
   static const String _devChannelPath = '/${_devChannelNamespace}';
   static const String _devServerClientFile = 'dev_server_client.js';
+  static final Logger _devLogger = new Logger('ButterflyDevServer');
 
   /// Starts a development server listening on the given [port] number.
   static Future<ButterflyDevServer> start(int port) async {
     final server = await HttpServer.bind(InternetAddress.LOOPBACK_IP_V4, port);
-    print('Butterfly dev server listening on http://localhost:$port');
+    _initLogger();
+    _devLogger.info('Butterfly dev server listening on http://localhost:$port');
     return new ButterflyDevServer._(server);
   }
 
@@ -50,7 +54,7 @@ class ButterflyDevServer {
 
   Future<Null> _listen() async {
     await for (final request in _server) {
-      print('[HTTP] ${request.method} ${request.uri}');
+      _devLogger.info('[HTTP] ${request.method} ${request.uri}');
       try {
         if (request.uri.path.startsWith(_devChannelPath)) {
           await _serveDevRequest(request);
@@ -58,8 +62,7 @@ class ButterflyDevServer {
           await _serveStatic(request);
         }
       } catch (error, stackTrace) {
-        stderr.writeln(error);
-        stderr.writeln(stackTrace);
+        _devLogger.shout('Error', error, stackTrace);
         final errorResponse = <String, String>{
           'error': '${error}\n${stackTrace}',
         };
@@ -163,10 +166,47 @@ class ButterflyDevServer {
           ContentType.parse(mime.lookupMimeType(path));
       await file.openRead().pipe(request.response);
     } else if (pathType == FileSystemEntityType.LINK) {
+      _devLogger
+          .warning('${request.uri.path}: Symlinks not supported at this time');
       request.response.statusCode = 500;
       request.response.writeln('Symlinks not supported at this time');
     } else {
       throw new StateError('Unsupported path type: ${pathType}');
     }
   }
+}
+
+/// Configures the logger to print messages to the command line.
+///
+/// In the future, this could be expanded to send logs to the browser
+/// dev console or a special addon.
+void _initLogger() {
+  final pen = new ansi.AnsiPen();
+  final welcomeMessage = r'''
+  ____          _    _                __  _        
+ |  _ \        | |  | |              / _|| |       
+ | |_) | _   _ | |_ | |_  ___  _ __ | |_ | | _   _ 
+ |  _ < | | | || __|| __|/ _ \| '__||  _|| || | | |
+ | |_) || |_| || |_ | |_|  __/| |   | |  | || |_| |
+ |____/  \__,_| \__| \__|\___||_|   |_|  |_| \__, |
+                                              __/ |
+                                             |___/  
+  dev server version 0.0.1.     
+''';
+  pen.magenta();
+  print(pen.write(welcomeMessage) + '\n\n');
+
+  Logger.root.level = Level.ALL;
+  Logger.root.onRecord.listen((LogRecord rec) {
+    pen.blue();
+    final timestamp = pen.write('${rec.time}:');
+    if (rec.level < Level.WARNING) {
+      pen.black();
+    } else if (rec.level > Level.WARNING) {
+      pen.yellow();
+    } else {
+      pen.red();
+    }
+    print('$timestamp ' + pen.write('${rec.message}'));
+  });
 }
