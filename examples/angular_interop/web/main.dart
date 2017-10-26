86b937c428d61cc7cb71dec6d021c9d8073f8e0a
jonahwilliams <campfish91@gmail.com>
2017-10-26 12:19:29 -0700

    initial working example of angular integration in butterfly

--- examples/angular_interop/web/main.dart
+++ examples/angular_interop/web/main.dart
@@ -1,22 +1,7 @@
 import 'dart:html' as html;
-import 'dart:async';
-
-import 'package:angular/angular.dart';
 import 'package:butterfly/butterfly.dart';
 import 'package:angular_interop/angular_interop.dart';
 
 void main() {
-  bootstrap(AppComponent);
-}
-
-@Component(selector: 'foo', template: '')
-class AppComponent implements OnInit {
-  final html.Element _element;
-
-  AppComponent(this._element);
-
-  @override
-  void ngOnInit() {
-    runApp(new App(), _element);
-  }
+  runApp(new App(), html.querySelector('#app-host'));
 }
--- package/butterfly_components/web/main.dart
+++
@@ -1,5 +0,0 @@
-import 'package:butterfly_components/butterfly_components.dart';
-
-void main() {
-  print(MaterialButtonComponent);
-}
