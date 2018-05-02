fca3b277cb6c5b6ec93928db594e79c170527ce3
Jonah Williams <jonahwilliams@google.com>
2018-05-02 16:23:54 -0700

    remove console.log

--- package/butterfly/lib/dev_server_client.js
+++ package/butterfly/lib/dev_server_client.js
@@ -40,7 +40,6 @@ export class ButterflyModule extends sync.ButterflyModuleBase {
       xhr.open('POST', `/_butterfly/${this.moduleName}/${method}`, false /* synchronous */);
       xhr.setRequestHeader("Content-Type", "text/plain;charset=UTF-8");
       xhr.send(JSON.stringify(args));
-      console.log(xhr.responseText);
       let result = JSON.parse(xhr.responseText);
       if (result != null && result.hasOwnProperty('error')) {
         throw result['error'];
